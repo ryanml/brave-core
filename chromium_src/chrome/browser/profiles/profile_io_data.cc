08e8436813846b3697819f939f898d94fe654148
Jocelyn Liu <yrliou@gmail.com>
2018-08-24 09:53:05 -0700

    Add magnet protocol handler in profile_io_data instead

---
+++ chromium_src/chrome/browser/profiles/profile_io_data.cc
@@ -0,0 +1,4 @@
+#include "brave/browser/net/brave_profile_network_delegate.h"
+#include "brave/components/brave_webtorrent/browser/net/magnet_protocol_handler.h"
+
+#include "../../../../../../chrome/browser/profiles/profile_io_data.cc"
--- components/brave_webtorrent/browser/net/magnet_protocol_handler.cc
+++ components/brave_webtorrent/browser/net/magnet_protocol_handler.cc
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "brave/net/url_request/magnet_protocol_handler.h"
+#include "brave/components/brave_webtorrent/browser/net/magnet_protocol_handler.h"
 
 #include "brave/common/network_constants.h"
 #include "base/strings/string_util.h"
