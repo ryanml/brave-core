99769962d43fda782a2b2de40f8646e2aebcdd47
Jocelyn Liu <yrliou@gmail.com>
2018-08-27 12:28:25 -0700

    Uninstall redux-logger

--- components/brave_webtorrent/extension/background/store.ts
+++ components/brave_webtorrent/extension/background/store.ts
@@ -2,27 +2,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import { applyMiddleware, createStore, Middleware } from 'redux'
+import { createStore } from 'redux'
 import { wrapStore } from 'react-chrome-redux'
-import { createLogger } from 'redux-logger'
 
 import reducers from './reducers'
 
-const logger = createLogger({
-  collapsed: true
-})
-
-const getMiddleware = () => {
-  const args: Middleware[] = []
-  if (process.env.NODE_ENV === `development`) {
-    args.push(logger)
-  }
-
-  return applyMiddleware(...args)
-}
-
-const initialState = {}
-const store = createStore(reducers, initialState, getMiddleware())
+const store = createStore(reducers)
 
 wrapStore(store, {
   portName: 'WEBTORRENT'
