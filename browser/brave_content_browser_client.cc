d139e68f99c7016686b652841dd7ce81bd69396f
Jocelyn Liu <yrliou@gmail.com>
2018-08-28 20:26:25 -0700

    Handle open link in the same page case and move magnet handler into brave_webtorrent component

--- browser/brave_content_browser_client.cc
+++ browser/brave_content_browser_client.cc
@@ -4,11 +4,10 @@
 
 #include "brave/browser/brave_content_browser_client.h"
 
-#include "base/strings/string_util.h"
 #include "brave/browser/brave_browser_main_extra_parts.h"
-#include "brave/common/url_constants.h"
 #include "brave/common/webui_url_constants.h"
 #include "brave/components/brave_shields/browser/brave_shields_web_contents_observer.h"
+#include "brave/components/brave_webtorrent/browser/content_browser_client_helper.h"
 #include "brave/components/content_settings/core/browser/brave_cookie_settings.h"
 #include "chrome/browser/content_settings/tab_specific_content_settings.h"
 #include "chrome/browser/profiles/profile_io_data.h"
@@ -16,7 +15,6 @@
 #include "content/browser/frame_host/render_frame_host_impl.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/browser_url_handler.h"
-#include "net/base/escape.h"
 
 using content::BrowserThread;
 using content::RenderFrameHost;
@@ -57,22 +55,6 @@ bool HandleURLReverseRewrite(GURL* url,
   return false;
 }
 
-bool HandleMagnetURLRewrite(GURL* url,
-                            content::BrowserContext* browser_context) {
-  if (url->SchemeIs(kMagnetScheme)) {
-    GURL extension_page_url(
-        "chrome-extension://lgjmpdmojkpocjcopdikifhejkkjglho/extension/brave_webtorrent.html?%s");
-    std::string translatedSpec(extension_page_url.spec());
-    base::ReplaceFirstSubstringAfterOffset(
-        &translatedSpec, 0, "%s",
-        net::EscapeQueryParamValue(url->spec(), true));
-    *url = GURL(translatedSpec);
-    return true;
-  }
-
-  return false;
-}
-
 WebContents* GetWebContents(int render_process_id, int render_frame_id) {
   RenderFrameHost* rfh =
       RenderFrameHost::FromID(render_process_id, render_frame_id);
@@ -99,10 +81,10 @@ void BraveContentBrowserClient::BrowserURLHandlerCreated(
     content::BrowserURLHandler* handler) {
   // Insert handler for chrome://newtab so that we handle it
   // before anything else can.
+  handler->AddHandlerPair(&webtorrent::HandleMagnetURLRewrite,
+                          content::BrowserURLHandler::null_handler());
   handler->AddHandlerPair(&HandleURLRewrite,
                           &HandleURLReverseRewrite);
-  handler->AddHandlerPair(&HandleMagnetURLRewrite,
-                          content::BrowserURLHandler::null_handler());
   ChromeContentBrowserClient::BrowserURLHandlerCreated(handler);
 }
 
@@ -139,6 +121,25 @@ BraveContentBrowserClient::AllowWebBluetooth(
     const url::Origin& requesting_origin,
     const url::Origin& embedding_origin) {
   return content::ContentBrowserClient::AllowWebBluetoothResult::BLOCK_GLOBALLY_DISABLED;
+}
+
+bool BraveContentBrowserClient::HandleExternalProtocol(
+    const GURL& url,
+    content::ResourceRequestInfo::WebContentsGetter web_contents_getter,
+    int child_id,
+    content::NavigationUIData* navigation_data,
+    bool is_main_frame,
+    ui::PageTransition page_transition,
+    bool has_user_gesture) {
+
+  if (webtorrent::HandleMagnetProtocol(url, web_contents_getter,
+        page_transition, has_user_gesture)) {
+    return true;
+  }
+
+  return ChromeContentBrowserClient::HandleExternalProtocol(
+      url, web_contents_getter, child_id, navigation_data, is_main_frame,
+      page_transition, has_user_gesture);
 }
 
 bool BraveContentBrowserClient::AllowSetCookie(
