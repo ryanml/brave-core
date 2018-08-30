3d01d44ce507ad7f80fc6de6c9a81ad0f91046ce
Jocelyn Liu <yrliou@gmail.com>
2018-08-30 10:35:10 -0700

    [WIP] fix build error because of brave-ui upgreade

--- components/brave_webtorrent/extension/components/torrentViewerHeader.tsx
+++ components/brave_webtorrent/extension/components/torrentViewerHeader.tsx
@@ -3,7 +3,7 @@
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import * as React from 'react'
-import { ButtonPrimary, ButtonSecondary, Column, Grid } from 'brave-ui/components'
+import { Button, Column, Grid } from 'brave-ui/components'
 import { TitleHeading } from 'brave-ui/old'
 
 // Constants
@@ -35,7 +35,10 @@ export default class TorrentViewerHeader extends React.PureComponent<Props, {}> 
   }
 
   render () {
-    const { name, torrent } = this.props
+    const { torrent } = this.props
+    const name = typeof(this.props.name) === 'object'
+      ? this.props.name[0]
+      : this.props.name
     const title = torrent
       ? name
       : name
@@ -45,22 +48,21 @@ export default class TorrentViewerHeader extends React.PureComponent<Props, {}> 
 
     return (
       <Grid>
-        <Column size={9} theme={theme.headerColumnLeft}>
+        <Column size={9} customStyle={theme.headerColumnLeft}>
           <TitleHeading
             text={title}
           />
         </Column>
-        <Column size={3} theme={theme.headerColumnRight}>
-          <ButtonPrimary
+        <Column size={3} customStyle={theme.headerColumnRight}>
+          <Button
+            type='accent'
             text={mainButtonText}
-            color='brand'
-            size='medium'
             onClick={this.onClick}
           />
-          <ButtonSecondary
+          <Button
+            type='accent'
+            level='secondary'
             text='Copy Magnet Link'
-            color='brand'
-            size='medium'
             onClick={this.onCopyClick}
           />
         </Column>
