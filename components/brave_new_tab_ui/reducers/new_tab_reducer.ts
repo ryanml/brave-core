5f9e57f174c243a4100151cac02202c3bb1eddf2
ryanml <ryanlanese@gmail.com>
2020-03-11 14:28:26 -0700

    NTP update

--- components/brave_new_tab_ui/reducers/new_tab_reducer.ts
+++ components/brave_new_tab_ui/reducers/new_tab_reducer.ts
@@ -308,6 +308,11 @@ export const newTabReducer: Reducer<NewTab.State | undefined> = (state: NewTab.S
       }
       break
 
+    case types.ON_BINANCE_USER_TLD:
+      state = { ...state }
+      state.binanceState.userTLD = payload.userTLD
+      break
+
     default:
       break
   }
